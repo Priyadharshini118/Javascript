17c1108072106b3d8a76054bf73fc7e924905fe0
priyadharshini118 <priyaraja1108ar@gmail.com>
2025-01-04 14:18:54 +0530

    2nd Commit

--- v2/script.js
+++ v2/script.js
@@ -2,3 +2,9 @@ let age =22;
 console.log(age);
 console.log(`Priyadharshini is ${age} years old.`);
 console.log(typeof age);
+
+let name ="Priyadharshini R";
+let designation ="SDE @ Zoho ";
+document.getElementById("myId1").textContent=name;
+document.getElementById("myId2").textContent=`I am a ${designation}`;
+
